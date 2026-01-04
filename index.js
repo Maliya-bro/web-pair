c0e630774f5fac5c2e889cfd25b09f9feb054153
Maliya-bro <darkmaliya10@gmail.com>
2026-01-04 14:12:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
-let code = require('./pair');
+
+__path = process.cwd();
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
 
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-
+/* ---------- Middleware ---------- */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-app.listen(PORT, '0.0.0.0', () => {
-console.log(`⏩ Server running on http://0.0.0.0:` + PORT)
+
+/* ---------- Routes ---------- */
+const code = require('./pair');
+app.use('/code', code);
+
+app.get('/', async (req, res) => {
+    res.sendFile(__path + '/pair.html');
 });
-module.exports = app
+
+/* ---------- Server ---------- */
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`⏩ Server running on http://0.0.0.0:${PORT}`);
+});
+
+module.exports = app;
