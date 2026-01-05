65c75a0f69ab5ee007b90ba71e594ceeae09b815
Maliya-bro <darkmaliya10@gmail.com>
2026-01-05 15:21:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
+const express = require('express');
 const app = express();
+
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
+__path = process.cwd();
+
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+/* ---------- Middleware ---------- */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).send("MALIYA-MD is running");
+/* ---------- Routes ---------- */
+const code = require('./pair');
+app.use('/code', code);
+
+app.get('/', async (req, res) => {
+    res.sendFile(__path + '/pair.html');
 });
 
-app.use("/code", require("./pair"));
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+/* ---------- Server ---------- */
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`⏩ Server running on http://0.0.0.0:${PORT}`);
 });
+
+module.exports = app;
